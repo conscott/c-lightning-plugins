f5f805a396ba7cd489a49553c267f98e6a444882
Conor Scott <conor.r.scott.88@gmail.com>
2019-01-14 19:24:34 +0400

    Add reserve balances

--- balance.py
+++ balance.py
@@ -23,10 +23,13 @@ def balance(plugin, unit):
 
     incoming_total_balance = 0
     outgoing_total_balance = 0
+    reserve_balance = 0
     for p in peerinfo['peers']:
         for channel in p['channels']:
             incoming_total_balance += (channel['msatoshi_total'] - channel['msatoshi_to_us'])
             outgoing_total_balance += channel['msatoshi_to_us']
+            reserve_balance += channel['our_channel_reserve_satoshis']
+
 
     paid = sum([p['msatoshi'] for p in payments['payments'] if p['status'] == 'complete'])
     paid_w_fees = sum([p['msatoshi_sent'] for p in payments['payments'] if p['status'] == 'complete'])
@@ -37,8 +40,10 @@ def balance(plugin, unit):
         'onchain_balance': '{0:.8f}'.format(onchain_value / COIN),
         'channel_balance': '{0:.8f}'.format(channel_value / COIN),
         'total_balance': '{0:.8f}'.format(total_funds / COIN),
-        'balance_outgoing': '{0:.8f}'.format(outgoing_total_balance / MCOIN),
-        'balance_incoming': '{0:.8f}'.format(incoming_total_balance / MCOIN),
+        'outgoing_balance': '{0:.8f}'.format(outgoing_total_balance / MCOIN),
+        'reserve_balance':  '{0:.8f}'.format(reserve_balance / MCOIN),
+        'spendable_balance': '{0:.8f}'.format((outgoing_total_balance - reserve_balance) / MCOIN),
+        'incoming_balance': '{0:.8f}'.format(incoming_total_balance / MCOIN),
         'total_invoices_paid': '{0:.8f}'.format(paid / MCOIN),
         'total_routing_fees_paid': '{0:.8f}'.format(fees / MCOIN),
         'total_paid_with_routing': '{0:.8f}'.format(paid_w_fees / MCOIN),
